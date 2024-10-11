21af8938e4fbdf6e17fe705162c2ef3cd2cdb87e
JAJesusGarcia <jesusjagarcia98@gmail.com>
2024-10-11 16:43:21 -0300

    bdd

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,5 +1,5 @@
 import { DataSource } from 'typeorm';
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './envs';
+import { DATABASE_URL } from './envs'; // Importamos la URL completa de la base de datos
 import { User } from '../entities/User';
 import { Credential } from '../entities/Credential';
 import { Order } from '../entities/Order';
@@ -8,14 +8,12 @@ import { Product } from '../entities/Product';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  synchronize: true,
-  //   dropSchema: true,
+  url: DATABASE_URL, // Usamos la URL completa
+  synchronize: true, // Sincroniza las entidades con la base de datos
   logging: false,
+  ssl: {
+    rejectUnauthorized: false, // Para evitar problemas de SSL en producción
+  },
   entities: [User, Credential, Order, Product, Category],
   subscribers: [],
   migrations: [],
