73869e3a26505165b2ddf23c9526b16c6a0b4e58
JAJesusGarcia <jesusjagarcia98@gmail.com>
2024-11-25 23:56:55 -0300

    dropSchema True

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -9,7 +9,7 @@ import { Product } from '../entities/Product';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: DATABASE_URL, // Usamos la URL completa
-  // dropSchema: false,
+  dropSchema: true,
   synchronize: true, // Sincroniza las entidades con la base de datos
   logging: false,
   ssl: {
